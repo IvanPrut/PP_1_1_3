a52940c54e81c38605247f3873060a7f0af8e731
IvanPrut <ivanprut@list.ru>
2024-10-31 23:16:03 +0300

    Modified Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,31 +3,27 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/pp113DB";
     private static final String USER = "rootroot";
     private static final String PASSWORD = "root";
 
-    private Connection connection;
+    private static Logger log = Logger.getLogger(Util.class.getName());
 
-    Util() throws SQLException {
+    private static Connection connection;
+
+    private Util() {}
+
+    public static Connection getConnection() {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            log.log(Level.INFO, "Connection established" );
         } catch (SQLException e) {
-            throw new SQLException(e);
+            log.log(Level.SEVERE, "Connection false" );
         }
-    }
-
-    public Connection getConnection() {
         return connection;
-    }
-
-    public void closeConnection() throws SQLException {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new SQLException(e);
-        }
     }
 }
