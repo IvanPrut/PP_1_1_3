5dd9979171c6f09cf33900ec2d3faaa19888964e
IvanPrut <ivanprut@list.ru>
2024-10-31 20:46:39 +0300

    Modified Util.java: add basic functionality

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/pp113DB";
+    private static final String USER = "rootroot";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    Util() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+    }
 }
