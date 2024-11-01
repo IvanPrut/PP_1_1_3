c762077566978d27d32fc1d44f3ab5b08957a3f9
IvanPrut <ivanprut@list.ru>
2024-11-01 11:29:12 +0300

    Modified UserDaoJDBCImpl.java: add basic functionality

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,22 +3,23 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    public static Logger log = Logger.getLogger(UserDaoJDBCImpl.class.getName());
+    public static final Logger log = Logger.getLogger(UserDaoJDBCImpl.class.getName());
 
-    public UserDaoJDBCImpl() {}
+    // По условию задачи требуется
+    public UserDaoJDBCImpl() {
+        // По условию задачи класс dao должен иметь конструктор пустой/по умолчанию
+    }
 
     public void createUsersTable() {
-        String query = "create table user (id BIGINT auto_increment primary key, name varchar(80) " +
+        String query = "create table if not exists user (id BIGINT auto_increment primary key, name varchar(80) " +
                 "not null, lastName varchar(80) not null, age tinyint not null);";
         try (Connection connection1 = Util.getConnection(); Statement statement1 = connection1.createStatement()) {
             statement1.executeUpdate(query);
@@ -28,7 +29,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        String query = "drop table user";
+        String query = "drop table if exists user";
         try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate(query);
         } catch (SQLException e) {
@@ -55,7 +56,22 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> result = new ArrayList<>();
+        String query = "select id, name, lastName, age from user";
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                result.add(user);
+            }
+        } catch (SQLException e) {
+            log.log(Level.SEVERE, "Error getting all users", e);
+        }
+        return result;
     }
 
     public void cleanUsersTable() {
