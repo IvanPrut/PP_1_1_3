c98445c09f0d8f9e68d0ab32c7964631c8dbc609
IvanPrut <ivanprut@list.ru>
2024-11-07 23:21:20 +0300

    Changed program structure by using hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,94 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private static final Logger log = Logger.getLogger(UserDaoHibernateImpl.class.getName());
+
+    public UserDaoHibernateImpl() {
+        //По условию задачи класс dao должен иметь конструктор пустой/по умолчанию
     }
 
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            String query = "create table if not exists user (id BIGINT auto_increment primary key, name varchar(80) " +
+                    "not null, lastName varchar(80) not null, age tinyint not null);";
+            session.createNativeMutationQuery(query).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error creating table", e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            String query = "drop table if exists user";
+            session.createNativeMutationQuery(query).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error dropping table", e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.persist(user);
+            session.getTransaction().commit();
+            System.out.println("User с именем — " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error saving user", e);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createMutationQuery("DELETE FROM User WHERE id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error removing user", e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            users = session.createQuery("from User", User.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error getting all users", e);
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createMutationQuery("delete from User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error cleaning table", e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/pp113DB";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp113db";
     private static final String USER = "rootroot";
     private static final String PASSWORD = "root";
 
     private static final Logger log = Logger.getLogger(Util.class.getName());
 
     private static Connection connection;
+    private static SessionFactory sessionFactory;
+
+    static {
+       try {
+           Configuration configuration = new Configuration();
+           configuration.setProperty("hibernate.connection.url", URL);
+           configuration.setProperty("hibernate.connection.username", USER);
+           configuration.setProperty("hibernate.connection.password", PASSWORD);
+           configuration.setProperty("current_session_context_class", "thread");
+           configuration.addAnnotatedClass(User.class);
+           ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                   .applySettings(configuration
+                           .getProperties())
+                   .build();
+           sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+       } catch (Exception ex) {
+           log.log(Level.SEVERE, ex.getMessage(), ex);
+       }
+    }
 
     private Util() {}
 
@@ -24,5 +49,9 @@ public class Util {
             log.log(Level.SEVERE, "Connection false" );
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
